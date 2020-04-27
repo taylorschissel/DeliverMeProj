27139198ab7f5e766a84795a61e0e0a0cac81a2d
Isaac Chapman <isaacfc4@bu.edu>
2020-04-27 18:50:42 +0000

    from pythonanywhere

--- DeliverApp/forms.py
+++ DeliverApp/forms.py
@@ -4,5 +4,29 @@ from .models import DeliveryRequest
 class DeliveryForm(forms.ModelForm):
     class Meta:
         model = DeliveryRequest
-        fields = ('item','description','pickupName','pickupStreetAddress','pickupCity','pickupState','pickupZipcode','dropoffName','dropoffStreetAddress','dropoffCity','dropoffState','dropoffZipcode',)
-
+        fields = ('item',
+                  'description',
+                  'pickupName',
+                  'pickupStreetAddress',
+                  'pickupCity',
+                  'pickupState',
+                  'pickupZipcode',
+                  'dropoffName',
+                  'dropoffStreetAddress',
+                  'dropoffCity',
+                  'dropoffState',
+                  'dropoffZipcode',)
+        labels = {
+            'item':'Item',
+          'description':'Description',
+          'pickupName':'Pickup Name',
+          'pickupStreetAddress':'Street Address',
+          'pickupCity':'City',
+          'pickupState':'State',
+          'pickupZipcode':'Zipcode',
+          'dropoffName':'Dropoff Name',
+          'dropoffStreetAddress':'Street Address',
+          'dropoffCity':'City',
+          'dropoffState':'State',
+          'dropoffZipcode':'Zipcode',
+        }
